c148bf8b197a8e0cd5fed22eec583294d43053da
gcorbel <guirec.corbel@gmail.com>
2012-05-16 19:38:25 -0400

    The comment owner can remove his comment

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe 'Comments' do
   let!(:project) { create(:project) }
-  let(:user1) { create(:user) }
+  let(:user) { create(:user) }
   let(:comment) do 
     create(:comment,
            message: 'My Message', 
@@ -45,17 +45,30 @@ describe 'Comments' do
   end
 
   describe 'Delete' do
+    before(:each) { sign_in user }
+
     context 'When the user signed in is the project owner' do
       it 'allow to delete comment' do
-        sign_in user1
-        comment.project.user = user1
+        comment.project.user = user
         comment.project.save
-        visit project_path(project)
-        within("#comment_#{comment.id}") do
-          click_link 'Supprimer'
-        end
-        page.should have_content('Votre commentaire a été supprimé')
+        delete_comment
       end
     end
+
+    context 'When the user signed in is the comment owner' do
+      it 'allow to delete the comment' do
+        comment.user = user
+        comment.save
+        delete_comment
+      end
+    end
+  end
+
+  def delete_comment
+    visit project_path(project)
+    within("#comment_#{comment.id}") do
+      click_link 'Supprimer'
+    end
+    page.should have_content('Votre commentaire a été supprimé')
   end
 end
