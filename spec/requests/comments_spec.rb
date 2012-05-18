60c16f8c79bf87ba87378293ac07d554f9b92596
gcorbel <guirec.corbel@gmail.com>
2012-05-18 06:11:14 -0400

    Add a context for valid data on creation

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -24,15 +24,16 @@ describe 'Comments' do
     context 'when not signed in' do
       before(:each) { visit project_path(project) }
 
-      it 'Enable to create a new comment' do
-        within('#new_comment') do
-          fill_in('Nom', with: 'My name')
-          select('General', from: 'Categorie')
-          fill_in('Message', with: 'My Message')
-          click_button 'Envoyer'
+      context 'with valid data' do
+        it 'Enable to create a new comment' do
+          within('#new_comment') do
+            fill_in('Nom', with: 'My name')
+            select('General', from: 'Categorie')
+            fill_in('Message', with: 'My Message')
+            click_button 'Envoyer'
+          end
+          page.should have_content('Votre commentaire a été ajouté')
         end
-
-        page.should have_content('Votre commentaire a été ajouté')
       end
 
       context 'with invalid data' do
