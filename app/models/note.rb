01f736343e0e4832660ed7d66cd0feae3a281b17
gcorbel <guirec.corbel@gmail.com>
2012-05-22 12:46:44 -0400

    change validation for notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,6 +3,8 @@ class Note < ActiveRecord::Base
   belongs_to :category
 
   validates :value, presence: true
+  validates :project, presence: true
+  validates :category, presence: true
 
   attr_accessible :value, :project_id, :category_id, :project, :category
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -5,4 +5,6 @@ describe Note do
   it { should belong_to(:category) }
 
   it { should validate_presence_of :value }
+  it { should validate_presence_of :project }
+  it { should validate_presence_of :category }
 end
