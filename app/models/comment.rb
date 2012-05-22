dfb53f8109c1dcc540436bccab9bff2f60488d46
gcorbel <guirec.corbel@gmail.com>
2012-05-22 12:45:04 -0400

    change validation for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,12 @@ class Comment < ActiveRecord::Base
   attr_accessible :message, :username, :category_id, :user_id
 
   validates :message, presence: true
+  validates :project, presence: true
+  validates :category, presence: true
+  validates :username, presence: true, unless: :user?
+
+  private 
+    def user?
+      user.present?
+    end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,4 +6,22 @@ describe Comment do
   it { should belong_to(:category) }
 
   it { should validate_presence_of(:message) }
+  it { should validate_presence_of(:project) }
+  it { should validate_presence_of(:category) }
+  it { should validate_presence_of(:username) }
+
+  context 'when the comment have a user' do
+    let(:project) { build(:project) }
+    let(:category) { build(:category) }
+    let(:user) { build(:user) }
+    let(:comment) { build(:comment,
+                           project: project,
+                           category: category,
+                           user: user,
+                           username: nil) }
+
+    it "don't validate presence of username" do
+      comment.should be_valid
+    end
+  end
 end
