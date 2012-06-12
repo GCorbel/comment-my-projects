d97b12c413dbed820d5d29e61f4a0e53147efeaa
Guirec Corbel <guirec.corbel@gmail.com>
2012-06-12 17:00:07 -0400

    Add the default url for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,5 @@ CommentMyProjects::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   GA.tracker = "UA-32598287-1"
+  config.action_mailer.default_url_options = { :host => 'comment-my-projects.heroku.com' }
 end
