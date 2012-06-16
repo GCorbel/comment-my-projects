6637098e75d4b9df401e29296278e12eb8881208
Guirec Corbel <guirec.corbel@gmail.com>
2012-06-16 04:46:22 -0400

    Add a to_s method to categories and use it

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe Category do
+  let(:category) { create(:category) }
+
   it { should have_many(:projects).through(:category_projects) }
   it { should have_many(:category_projects) }
+
+  describe :to_s do
+    subject { category.to_s }
+    
+    it { should == category.label }
+  end
 end
