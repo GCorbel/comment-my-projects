84dd45231d4f30615fd8e1591c897fb585d426c8
Guirec Corbel <guirec.corbel@gmail.com>
2012-06-17 11:22:01 -0400

    Refactor the markdown input

--- app/inputs/markdown_input.rb
+++ app/inputs/markdown_input.rb
@@ -1,16 +1,14 @@
 class MarkdownInput < SimpleForm::Inputs::Base
   def input
-    out = template.content_tag(:div, class: 'wmd-panel') do
-      out2 = template.content_tag(:div, '', id: 'wmd-button-bar')
-      out2 << @builder.text_area(attribute_name,
-                                 id: 'wmd-input', 
-                                 class: 'wmd-input')
-      out2.html_safe
-    end
-    out << template.content_tag(:div,
-                                '',
-                                id: 'wmd-preview',
-                                class: 'wmd-panel wmd-preview')
-    out.html_safe
+    template.content_tag(:div, class: 'wmd-panel') do
+      template.content_tag(:div, '', id: 'wmd-button-bar') +
+      @builder.text_area(attribute_name,
+                         id: 'wmd-input', 
+                         class: 'wmd-input')
+    end +
+    template.content_tag(:div,
+                         '',
+                         id: 'wmd-preview',
+                         class: 'wmd-panel wmd-preview')
   end
 end
