0c51cabb2e5f1cf496713f243c688c20fab54382
Guirec Corbel <guirec.corbel@gmail.com>
2012-06-19 19:37:32 -0400

    Marge customs markdown options with standard options

--- app/inputs/markdown_input.rb
+++ app/inputs/markdown_input.rb
@@ -1,10 +1,9 @@
 class MarkdownInput < SimpleForm::Inputs::Base
   def input
+    input_html_options.merge!({id: 'wmd-input'})
     template.content_tag(:div, class: 'wmd-panel') do
       template.content_tag(:div, '', id: 'wmd-button-bar') +
-      @builder.text_area(attribute_name,
-                         id: 'wmd-input', 
-                         class: 'wmd-input')
+      @builder.text_area(attribute_name, input_html_options)
     end +
     template.content_tag(:div,
                          '',
