02cce11c75838b4e67367766574b1ac566e43f64
Guirec Corbel <guirec.corbel@gmail.com>
2012-06-23 07:38:00 -0400

    Remove an useless migration

--- db/migrate/20120613151730_acts_as_commentable_with_threading_migration.rb
+++
@@ -1,9 +0,0 @@
-class ActsAsCommentableWithThreadingMigration < ActiveRecord::Migration
-  def change 
-    change_table :comments do |t|
-      t.integer :commentable_id, :default => 0
-      t.string :commentable_type, :default => ""
-      t.integer :parent_id, :lft, :rgt
-    end
-  end
-end
