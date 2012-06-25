95ff9a5ae36fdf447d63ff0d280c8b6a34d958bc
Guirec Corbel <guirec.corbel@gmail.com>
2012-06-24 21:14:53 -0400

    Redirect 404 error to root url

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ CommentMyProjects::Application.routes.draw do
 
   get "home/index"
 
+  match '*a', to: "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
