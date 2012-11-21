0fdd1432a4b85f09448c38e3e588b7c020a65477
gcorbel <guirec.corbel@gmail.com>
2012-11-20 20:48:57 -0500

    remove things related to spam from comments model specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -18,31 +18,4 @@ describe Comment do
                         category: category,
                         user: user,
                         username: nil) }
-
-  describe :request do
-    it 'retrive information from request' do
-      request = stub(remote_ip: '1.1.1.1',
-                     env: {"HTTP_USER_AGENT" => "User Agent", "HTTP_REFERER" => "Referer"})
-      comment.request = request
-      comment.user_ip.should == '1.1.1.1'
-      comment.user_agent.should == 'User Agent'
-      comment.referrer.should == 'Referer'
-    end
-  end
-
-  describe :after_create do
-    it "mark as a spam if it's a spam" do
-      comment = build(:comment, project: project, category: category)
-      comment.approved = true
-      comment.expects(:spam?).returns(true)
-      comment.save
-      comment.approved.should == false
-    end
-
-    context 'when the comment have a user' do
-      it "don't validate presence of username" do
-        comment.should be_valid
-      end
-    end
-  end
 end
