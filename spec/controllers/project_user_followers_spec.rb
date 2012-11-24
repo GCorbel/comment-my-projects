6921d61a26ab4b9ca377a5bd6e975a20b3c841f1
gcorbel <guirec.corbel@gmail.com>
2012-11-23 19:19:55 -0500

    reorganize stubs

--- spec/controllers/project_user_followers_spec.rb
+++ spec/controllers/project_user_followers_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe ProjectUserFollowersController do
   let(:project) { build_stubbed(:project) }
   let(:user) { build_stubbed(:user) }
-  let(:project_user_follower) { build_stubbed(:project_user_follower) }
-  let(:followers) { stub(find: user, new: user) }
   let(:args) do
     { project_id: project.id, format: :js, user_id: user.id }
   end
@@ -12,12 +10,7 @@ describe ProjectUserFollowersController do
   before do
     sign_in user
     Project.stubs(:find).returns(project)
-    project.stubs(:followers).returns(followers)
-    project.stubs(:add_follower)
     project.stubs(:remove_follower)
-    project.stubs(:to_s).returns(project.title)
-    User.stubs(:find).returns(user)
-    ProjectUserFollower.stubs(:find).returns(project_user_follower)
   end
 
   after { subject }
