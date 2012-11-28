3a8ad5b4c18047bff1a38d72919f62349d5f67af
gcorbel <guirec.corbel@gmail.com>
2012-11-27 20:31:33 -0500

    refactor the observers specs

--- spec/observers/comment_mailer_observer_spec.rb
+++ spec/observers/comment_mailer_observer_spec.rb
@@ -1,37 +1,31 @@
 describe CommentMailerObserver do
 
-  let(:category) { build(:category) }
-  let(:user) { build(:user) }
-  let(:user2) { build(:user) }
-  let(:user3) { build(:user) }
-  let(:project) { build(:project) }
-  let(:comment1) { build(:comment,
-                         item: project,
-                         user: user,
-                         username: nil) }
-  let(:comment2) { build(:comment, username: nil) }
+  let(:user) { build_stubbed(:user) }
+  let(:user2) { build_stubbed(:user) }
+  let(:project) { build_stubbed(:project, user: user2) }
+  let(:comment) { build(:comment, item: project, user: user, username: nil) }
   let(:mailer) { stub(:deliver) }
+
+  before do
+    project.stubs(:followers_ids).returns([user2.id])
+    User.stubs(:find).returns(user2)
+  end
+
+  after { comment.save! }
 
   describe :after_create do
     it "send an email to the project owner" do
-      project.user = user2
-      project.save!
       CommentMailer.expects(:comment_notify_item_owner)
                    .with(user2, project)
                    .returns(mailer)
-      comment1.save
     end
-  end
 
-  context 'when there is followers' do
-    it 'send an email to all the followers' do
-      project.user = user2
-      project.save
-      project.add_follower(user2)
-      CommentMailer.expects(:comment_notify_followers)
-                   .with(user2, project)
-                   .returns(mailer)
-      comment1.save
+    context 'when there is followers' do
+      it 'send an email to all the followers' do
+        CommentMailer.expects(:comment_notify_followers)
+                     .with(user2, project)
+                     .returns(mailer)
+      end
     end
   end
 end
