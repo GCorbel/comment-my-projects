15b09bf1aabba7ac4bbfe15ae28f45dcdf5e9383
gcorbel <guirec.corbel@gmail.com>
2012-11-28 17:51:20 -0500

    an administrator can manage all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,29 +4,33 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user
 
-    can :read, :all
-    can :create, Comment
-    can :destroy, Comment do |comment|
-      comment.user == user || comment.item.user == user
-    end
+    if user.admin
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Comment
+      can :destroy, Comment do |comment|
+        comment.user == user || comment.item.user == user
+      end
 
-    can :read, Project
-    can :create, Project
-    can :manage, Project do |project|
-      project.user == user
-    end
+      can :read, Project
+      can :create, Project
+      can :manage, Project do |project|
+        project.user == user
+      end
 
-    can :create, CategoryProject do |category_project|
-      category_project.project.user == user
-    end
-    can :manage, CategoryProject, project: { user_id: user.id }
+      can :create, CategoryProject do |category_project|
+        category_project.project.user == user
+      end
+      can :manage, CategoryProject, project: { user_id: user.id }
 
-    can :create, Note
+      can :create, Note
 
-    can :manage, ProjectUserFollower
+      can :manage, ProjectUserFollower
 
-    can :manage, Actuality do |actuality|
-      actuality.project.user == user
+      can :manage, Actuality do |actuality|
+        actuality.project.user == user
+      end
     end
   end
 end
