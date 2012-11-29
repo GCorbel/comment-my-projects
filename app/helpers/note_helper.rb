c78cd932027421729890da941aeade9ad7177f67
gcorbel <guirec.corbel@gmail.com>
2012-11-29 11:26:48 -0500

    refactor the note helper

--- app/helpers/note_helper.rb
+++ app/helpers/note_helper.rb
@@ -1,27 +1,27 @@
 module NoteHelper
   def note_for(project, category)
     content_tag(:div, class: 'notes') do
+      note = project.note_for(category)
+      number_of_notes = project.number_of_notes_for(category)
+      number_of_notes_plural = pluralize(number_of_notes, 'vote')
       raw(
-        category.label +
-        ' : ' +
-        if project.number_of_notes_for(category) == 0
+        "#{category.label} : " +
+        if number_of_notes == 0
           'Aucun vote'
         else
-          project.note_for(category).to_i.times.collect do
-            content_tag(:span, '', class: 'star true')
-          end.join +
-          (4 - project.note_for(category).to_i).times.collect do
-            content_tag(:span, '', class: 'star false')
-          end.join +
-          ' (' +
-          project.note_for(category).to_s +
-          '/4 - ' +
-          pluralize(
-            project.number_of_notes_for(category), 'vote'
-          ) +
-          ')'
+          stars(note, 'true') +
+          stars(4 - note, 'false') +
+          " (#{note}/4 - #{number_of_notes_plural})"
         end
       )
     end
+  end
+
+  private
+
+  def stars(note, css)
+    note.to_i.times.collect do
+      content_tag(:span, '', class: "star #{css}")
+    end.join
   end
 end
