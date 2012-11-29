c2798747fc88c8098edbf7d08569006e1601fe29
gcorbel <guirec.corbel@gmail.com>
2012-11-29 14:13:05 -0500

    refactor the search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -21,24 +21,31 @@ class Search
   end
 
   def project_text_search
+    projects = Project.select('projects.id, projects.title')
+      .select('MIN(comments.message) as comment_message')
+      .select('MIN(category_projects.description) as category_description')
+      .joins("LEFT OUTER JOIN comments ON comments.item_id = projects.id")
+      .joins("LEFT OUTER JOIN category_projects ON category_projects.project_id = projects.id")
+      .group("projects.id")
+      .order("projects.updated_at DESC")
+
+    projects = projects.where(type_id: project_type) if project_type.present?
+    add_conditions_for(projects, category)
+  end
+
+  private
+  def add_conditions_for(projects, category)
     word = "%#{text}%"
     conditions = 'projects.title ilike :word'
-    projects = Project.select('projects.id, projects.title')
-    projects = projects.group("projects.id")
 
     if category == PROJECT_COMMENT || category == PROJECT_ALL
-      projects = projects.select('MIN(comments.message) as comment_message')
-                         .joins("LEFT OUTER JOIN comments ON comments.item_id = projects.id")
       conditions += " or comments.message ilike :word"
     end
 
     if category == PROJECT_DESCRIPTION || category == PROJECT_ALL
-      projects = projects.select('MIN(category_projects.description) as category_description')
-                         .joins("LEFT OUTER JOIN category_projects ON category_projects.project_id = projects.id")
       conditions += ' or category_projects.description ilike :word'
     end
 
-    projects = projects.where(type_id: project_type) if project_type.present?
-    projects.where(conditions, word: word).order("projects.updated_at DESC")
+    projects.where(conditions, word: word)
   end
 end
