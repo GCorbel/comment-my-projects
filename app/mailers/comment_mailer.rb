087b905105ca052cc83be9ed7779f86a1e580c55
gcorbel <guirec.corbel@gmail.com>
2012-11-29 15:08:25 -0500

    remove some duplication in the mailer

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -3,22 +3,22 @@ class CommentMailer < ActionMailer::Base
   default from: "guirec.corbel@gmail.com"
 
   def comment_notify_item_owner(user, item)
-    @user = user
-    @item = item
     @prefix = "mailers.#{item.class.to_s.downcase}_comment_notify_item_owner"
-    @url = url_for(item)
-    mail(to: user.email, subject: subject)
+    prepare_mail(user, item)
   end
 
   def comment_notify_followers(user, item)
-    @user = user
-    @item = item
     @prefix = "mailers.#{item.class.to_s.downcase}_comment_notify"
-    @url = url_for(item)
-    mail(to: user.email, subject: subject)
+    prepare_mail(user, item)
   end
 
   private
+  def prepare_mail(user, item)
+    @user = user
+    @item = item
+    @url = url_for(item)
+    mail(to: user.email, subject: subject)
+  end
 
   def subject
     t("#{@prefix}_title")
