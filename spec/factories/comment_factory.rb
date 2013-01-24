e8390c1d24f93e06a3dda0512177a0e1b893bc58
gcorbel <guirec.corbel@gmail.com>
2013-01-24 05:46:21 -0500

    replace after_* by after(*)

--- spec/factories/comment_factory.rb
+++ spec/factories/comment_factory.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     user_ip "10.10.10.10"
     user_agent "Mozilla"
     referrer "www.test.com"
-    after_build { |comment| comment.stubs(:spam?).returns(false) }
-    after_create { |comment| comment.stubs(:spam?).returns(false) }
+    after(:build) { |comment| comment.stubs(:spam?).returns(false) }
+    after(:create) { |comment| comment.stubs(:spam?).returns(false) }
   end
 end
