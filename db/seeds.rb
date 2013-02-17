18481ea75339f74f2a032230701b7a1c28250df3
gcorbel <guirec.corbel@gmail.com>
2013-02-17 08:18:38 -0500

    Add the home page to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Page.create(title: 'Home', body: 'Home', home: true, locale: 'en')
+Page.create(title: 'Accueil', body: 'Accueil', home: true, locale: 'fr')
 ["Ruby", "Ruby On Rails", "Flash", "Flex", "ASP", "C", "C#", "C++", "Java", "Visual Basic", "Delphi", "Javascript", "Zend", "Synfony", "Cake", "PHP", "Python", "QT"].each do |name|
   ActsAsTaggableOn::Tag.create! name: name
 end
